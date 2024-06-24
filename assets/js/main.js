554812974bb3fbd32a583a2a9d2ae089657b7f6c
WesleyMut8 <wesley@mut.log.br>
2024-06-24 20:33:56 -0300

    refactor: Header mobile
    - toda lógica de estilização, estrutura e funcionamento do headerMobile foi alterada

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,40 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuHamburger = document.querySelector(".menu-hamburger");
-  const body = document.body;
-  const navigation = document.querySelector(".navigation");
+    var header           = document.getElementById('header');
+    var headerNavigation = document.getElementById('header-navigation');
+    var content          = document.getElementById('main-content');
+    var background       = document.getElementById('background');
+    var showSidebar      = false;
 
-  menuHamburger.addEventListener("click", function (e) {
-    e.preventDefault();
-    body.classList.toggle("menu-active");
-    navigation.classList.toggle("active");
-    menuHamburger.classList.toggle("close");
-  });
-
-  menuHamburger.addEventListener("keypress", function (e) {
-    if (e.key === "Enter") {
-      body.classList.toggle("menu-active");
-      navigation.classList.toggle("active");
-      menuHamburger.classList.toggle("close");
+    function toggleSidebar()
+    {
+        showSidebar = !showSidebar;
+        if(showSidebar)
+        {
+            headerNavigation.style.marginLeft = '-5vw';
+            headerNavigation.style.animationName = 'showSidebar';
+            content.style.filter = 'blur(2px)';
+            background.style.filter = 'blur(2px)';
+        }
+        else
+        {
+            headerNavigation.style.marginLeft = '-100vw';
+            headerNavigation.style.animationName = 'closeSidebar';
+            content.style.filter = '';
+        }
     }
-  });
 
-  document.addEventListener("click", function (event) {
-    const dropdowns = document.querySelectorAll('.has-dropdown');
-    dropdowns.forEach(function (dropdown) {
-      if (!dropdown.contains(event.target)) {
-        dropdown.classList.remove('active');
-      }
+    function closeSidebar()
+    {
+        if(showSidebar)
+        {
+            showSidebar = true;
+            toggleSidebar();
+        }
+    }
+
+    window.addEventListener('resize', function(event) {
+        if(window.innerWidth > 768 && showSidebar) 
+        {  
+            showSidebar = true;
+            toggleSidebar();
+        }
     });
-  });
-
-  const navItems = document.querySelectorAll("nav ul li");
-  navItems.forEach(function (item) {
-    item.addEventListener("click", function () {
-      if (window.innerWidth >= 768) {
-        navItems.forEach(function (sibling) {
-          sibling.classList.remove('active');
-        });
-        item.classList.toggle('active');
-      }
-    });
-  });
-
-  navItems.forEach(function (item) {
-    item.addEventListener("mouseenter", function () {
-      if (window.innerWidth >= 768) {
-        navItems.forEach(function (sibling) {
-          sibling.classList.remove('active');
-        });
-        item.classList.add('active');
-      }
-    });
-
-    item.addEventListener("mouseleave", function () {
-      if (window.innerWidth >= 768) {
-        item.classList.remove('active');
-      }
-    });
-  });
-
-  const aosScript = document.createElement('script');
-  aosScript.src = 'https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js';
-  aosScript.onload = function () {
-    AOS.init({ once: true });
-  };
-  document.head.appendChild(aosScript);
-});
