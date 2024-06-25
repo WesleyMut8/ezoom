4a23ac529af7231eb38225b6c3a845a85d2c0a50
WesleyMut8 <wesley@mut.log.br>
2024-06-25 13:18:36 -0300

    feat: Card header
    - criação do card de base para o header Desktop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,40 +1,44 @@
-    var header           = document.getElementById('header');
-    var headerNavigation = document.getElementById('header-navigation');
-    var content          = document.getElementById('main-content');
-    var background       = document.getElementById('background');
-    var showSidebar      = false;
+// Script for header navigation
 
-    function toggleSidebar()
+var header           = document.getElementById('header');
+var headerNavigation = document.getElementById('header-navigation');
+var content          = document.getElementById('main-content');
+var background       = document.getElementById('background');
+var showSidebar      = false;
+
+function toggleSidebar()
+{
+    showSidebar = !showSidebar;
+    if(showSidebar)
     {
-        showSidebar = !showSidebar;
-        if(showSidebar)
-        {
-            headerNavigation.style.marginLeft = '-10vw';
-            headerNavigation.style.animationName = 'showSidebar';
-            content.style.filter = 'blur(2px)';
-            background.style.filter = 'blur(2px)';
-        }
-        else
-        {
-            headerNavigation.style.marginLeft = '-100vw';
-            headerNavigation.style.animationName = 'closeSidebar';
-            content.style.filter = '';
-        }
+        headerNavigation.style.marginLeft = '-10vw';
+        headerNavigation.style.animationName = 'showSidebar';
+        content.style.filter = 'blur(2px)';
+        background.style.filter = 'blur(2px)';
     }
-
-    function closeSidebar()
+    else
     {
-        if(showSidebar)
-        {
-            showSidebar = true;
-            toggleSidebar();
-        }
+        headerNavigation.style.marginLeft = '-100vw';
+        headerNavigation.style.animationName = 'closeSidebar';
+        content.style.filter = '';
     }
+}
 
-    window.addEventListener('resize', function(event) {
-        if(window.innerWidth > 768 && showSidebar) 
-        {  
-            showSidebar = true;
-            toggleSidebar();
-        }
-    });
+function closeSidebar()
+{
+    if(showSidebar)
+    {
+        showSidebar = true;
+        toggleSidebar();
+    }
+}
+
+window.addEventListener('resize', function(event) {
+    if(window.innerWidth > 768 && showSidebar) 
+    {  
+        showSidebar = true;
+        toggleSidebar();
+    }
+});
+
+// Script for carousel
